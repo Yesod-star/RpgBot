8a2e1947f063e11e55dfb91d9d1001bfed43e6d9
Yesod-star <76537403+Yesod-star@users.noreply.github.com>
2022-09-29 23:28:32 -0300

    Add project files.

---
+++ RpgBot/Modules/Dice.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpgBot.Modules
+{
+    public class Dice
+    {
+        string lastNumberChar = "";
+
+        public int DiceRoll(string arg)
+        {
+            int value = 0;
+            char n;
+            int lastChar = 0;
+            char lastCharType = 'x';
+            int nDices = 0;
+            int finalNumber;
+            Random numAleatorio = new Random();
+
+            for (int cont = 0; cont < arg.Length; cont++)
+            {
+                int lastNumber = 0;
+                lastNumberChar = "";
+                n = arg[cont];
+                if (n=='d'||n=='+'||n=='-')
+                {
+                    switch (arg[cont])
+                    {
+                        case 'd':
+                            for (int contN =  lastChar+1; contN < cont; contN++)
+                            {
+                                lastNumberChar = (string)(lastNumberChar + arg[contN]);
+                            }
+                            int.TryParse(lastNumberChar, out nDices);
+                            lastCharType = 'd';
+                            break;
+                        case '+':
+                            for (int contN = lastChar + 1; contN < cont; contN++)
+                            {
+                                lastNumberChar = (string)(lastNumberChar + arg[contN]);
+                            }
+                            int.TryParse(lastNumberChar, out lastNumber);
+                            if (lastCharType == 'd')
+                            {
+                                value = nDices * numAleatorio.Next(1, 21);
+                            }
+                            else
+                            {
+                                if(lastCharType == '-')
+                                {
+                                    value =  value - lastNumber;
+                                }
+                                else
+                                {
+                                    value = -10000;
+                                }
+                            }
+                            lastCharType = '+';
+                            break;
+                        case '-':
+                            for (int contN = lastChar + 1; contN < cont; contN++)
+                            {
+                                lastNumberChar = (string)(lastNumberChar + arg[contN]);
+                            }
+                            int.TryParse(lastNumberChar, out lastNumber);
+                            if (lastCharType == 'd')
+                            {
+                                value = nDices * numAleatorio.Next(1, 21);
+                            }
+                            else
+                            {
+                                if (lastCharType == '+')
+                                {
+                                    value = value + lastNumber;
+                                }
+                                else
+                                {
+                                    value = -10000;
+                                }
+                            }
+                            lastCharType = '-';
+                            break;
+                        default:
+                            value = -999999;
+                            break;
+                    }
+                    lastChar = cont;
+                }
+            }
+
+            switch (lastCharType)
+            {
+                case 'd':
+                    for (int contN = lastChar + 1; contN < arg.Length; contN++)
+                    {
+                        lastNumberChar = (string)(lastNumberChar + arg[contN]);
+                    }
+                    int.TryParse(lastNumberChar, out finalNumber);
+                    value = nDices * numAleatorio.Next(1, 21);
+                    break;
+                case '+':
+                    for (int contN = lastChar + 1; contN < arg.Length; contN++)
+                    {
+                        lastNumberChar = (string)(lastNumberChar + arg[contN]);
+                    }
+                    int.TryParse(lastNumberChar, out finalNumber);
+                    value = value + finalNumber;
+                    break;
+                case '-':
+                    for (int contN = lastChar + 1; contN < arg.Length; contN++)
+                    {
+                        lastNumberChar = (string)(lastNumberChar + arg[contN]);
+                    }
+                    int.TryParse(lastNumberChar, out finalNumber);
+                    value = value - finalNumber;
+                    break;
+                default:
+                    value = -999999;
+                    break;
+            }
+            return value;
+        }
+    }
+}
---
+++ RpgBot/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using RpgBot.Modules;
+
+namespace RpgBot
+{
+    class RpgBot : ModuleBase<SocketCommandContext>
+    {
+        private readonly DiscordSocketClient _client;
+
+        Dice dice = new Dice();
+
+        static void Main(string[] args)=> new RpgBot().MainAsync().GetAwaiter().GetResult();
+
+        public RpgBot()
+        {
+            var config = new DiscordSocketConfig
+            {
+                GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent
+            };
+
+            _client = new DiscordSocketClient(config);
+
+            _client.Log += LogAsync;
+            _client.Ready += ReadyAsync;
+            _client.MessageReceived += MessageReceivedAsync;
+        }
+
+        public async Task MainAsync()
+        {
+            await _client.LoginAsync(TokenType.Bot, "MTAyMzIyMTA0NDI0NjQ5NTM2Ng.GGg7WW.at0MckKzHrOoc8idNt5xG7GfGWU5SHc8_Ep0fM");
+
+            await _client.StartAsync();
+
+            await Task.Delay(Timeout.Infinite);
+        }
+
+        private Task LogAsync(LogMessage log)
+        {
+            Console.WriteLine(log.ToString());
+            return Task.CompletedTask;
+        }
+        private Task ReadyAsync()
+        {
+            return Task.CompletedTask;
+        }
+        private async Task MessageReceivedAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+
+            if (message.Author.IsBot)
+                return;
+
+            int argPos = 0;
+            if (message.HasStringPrefix("!",ref argPos))
+            {
+                int value = dice.DiceRoll(message.Content);
+                if(value == -999999)
+                {
+                    await message.Channel.SendMessageAsync("Seu resultado contem um erro");
+                }
+                else
+                {
+                    if (value == -10000)
+                    {
+                        await message.Channel.SendMessageAsync("Siga o exemplo !XdX+X-X, sendo X qualquer numero");
+                    }
+                    else
+                    {
+                        await message.Channel.SendMessageAsync("Seu dado rolou " + value);
+                    }
+                }
+            }
+            else
+            {
+                await message.Channel.SendMessageAsync("Seu resultado contem um erro");
+            }
+        }
+    }
+}
